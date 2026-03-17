eb6627b85604086201b7d9da01c7b096a6951cff
adhammohamed123 <adhammo909@gmail.com>
2026-03-17 20:08:52 +0200

    feat: batch notification service worker to more resource optimization

--- OrderFlow.NotificationWorker/PaymentProcessedConsumer.cs
+++ OrderFlow.NotificationWorker/PaymentProcessedConsumer.cs
@@ -3,9 +3,9 @@ using OrderFlow.Contracts.Events.Payment;
 
 namespace OrderFlow.NotificationWorker
 {
-    public class PaymentProcessedConsumer : IConsumer<PaymentProcessed>
+    public class PaymentProcessedConsumer : IConsumer<Batch<PaymentProcessed>>
     {
-        public async Task Consume(ConsumeContext<PaymentProcessed> context)
+        private static async Task Consume(ConsumeContext<PaymentProcessed> context)
         {
             Console.WriteLine($"We Now Sending Notification to {context.Message.CustomerEmail}");
             Console.WriteLine($"TO: {context.Message.CustomerEmail}");
@@ -14,6 +14,22 @@ namespace OrderFlow.NotificationWorker
             Console.WriteLine($"we notify you for that payment processed sucessfully for order {context.Message.OrderId}");
             Console.WriteLine($"we received :{context.Message.TotalAmount} {context.Message.Currency}");
             await Task.CompletedTask;
+        }
+
+        public async Task Consume(ConsumeContext<Batch<PaymentProcessed>> context)
+        {
+            foreach (var consumeContext in context.Message)
+            {
+                await Consume(consumeContext);
+            }
+        }
+    }
+
+    public class PaymentProcessedConsumerDefinition : ConsumerDefinition<PaymentProcessedConsumer>
+    {
+        protected override void ConfigureConsumer(IReceiveEndpointConfigurator endpointConfigurator, IConsumerConfigurator<PaymentProcessedConsumer> consumerConfigurator, IRegistrationContext context)
+        {
+            consumerConfigurator.Options<BatchOptions>(options => options.SetConcurrencyLimit(2).SetMessageLimit(100).SetTimeLimit(TimeSpan.FromMinutes(2)));
         }
     }
 }
