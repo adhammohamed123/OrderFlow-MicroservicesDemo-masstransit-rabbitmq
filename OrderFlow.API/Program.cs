4ae0e5103fc3ad2e81416c1ecd9bceb30db1615c
adhammohamed123 <adhammo909@gmail.com>
2026-03-27 16:20:15 +0200

    simple Quartz Dashboard using CrystalQuartz

--- OrderFlow.API/Program.cs
+++ OrderFlow.API/Program.cs
@@ -1,51 +1,66 @@
 using MassTransit;
-using OrderFlow.Contracts.Commands;
-using OrderFlow.Contracts.Events;
-using OrderFlow.Contracts.Events.Order;
 using OrderFlow.Infrastracture;
+using Quartz;
+using CrystalQuartz.AspNetCore; // تأكد من إضافة الـ using ده
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+// 1. الإعدادات العادية
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddInfrastructure();
+
+// 2. إعداد MassTransit (كما هو لديك)
 builder.Services.AddMassTransit(buscfg =>
 {
-    //buscfg.SetKebabCaseEndpointNameFormatter(); 
     buscfg.AddEntityFrameworkOutbox<ApplicationDbContext>(cfg =>
     {
         cfg.UseSqlServer();
-        cfg.UseBusOutbox();// api layer
+        cfg.UseBusOutbox();
         cfg.DuplicateDetectionWindow = TimeSpan.FromMinutes(30);
         cfg.QueryDelay = TimeSpan.FromSeconds(10);
     });
+
     buscfg.UsingRabbitMq((buscontext, rabbitbusfactorycfgtor) =>
     {
         rabbitbusfactorycfgtor.Host("localhost", "masstransit", hostcfg =>
         {
             hostcfg.Username("test");
             hostcfg.Password("123");
-
         });
-        //rabbitbusfactorycfgtor.SendTopology.UseCorrelationId<OrderCreated>(o => o.Id);
     });
+});
 
+// 3. إعداد Quartz للاتصال بالداتابيز
+builder.Services.AddQuartz(q =>
+{
+    q.SchedulerName = "Runner"; // لازم يطابق اسم الـ Scheduler في الـ Worker
+    q.UsePersistentStore(p =>
+    {
+        var connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Masstransit;Integrated Security=True;Trust Server Certificate=True;";
+        p.UseSqlServer(connectionString);
+        p.UseNewtonsoftJsonSerializer();
+    });
 });
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// 4. إعداد الـ Pipeline
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
+
+// 5. تفعيل CrystalQuartz UI
+// بنجيب الـ Scheduler من الـ DI ونمرره للـ UI
+var schedulerFactory = app.Services.GetRequiredService<ISchedulerFactory>();
+var scheduler = schedulerFactory.GetScheduler().GetAwaiter().GetResult();
+
+// المسار الافتراضي هيكون /quartz
+app.UseCrystalQuartz(() => scheduler);
 
 app.MapControllers();
 
